710e2e2a28deee6b5f1bdbf3b29e059bc41e46c3
sevendaystoglory <2this0username0isnt2allowed@gmail.com>
2025-01-19 07:34:45 +0530

    Added orbits for ISS & moon

--- src/components/atmosphere.js
+++ src/components/atmosphere.js
@@ -3,7 +3,7 @@ import './atmosphere.css';
 
 const EarthAtmosphere = () => {
   const [zoomLevel, setZoomLevel] = useState(1);
-
+  const earhOrbit = 6387;
   const handleZoomChange = (event) => {
     // Apply logarithmic scaling to make zooming smoother
     const value = parseFloat(event.target.value);
@@ -40,25 +40,28 @@ const EarthAtmosphere = () => {
 
 
   const zoomStyle = {// Values are fetched from data/atmosphere
-    '--earth-size': `${6378 * zoomLevel}vh`,
-    '--troposphere-size': `${6390 * zoomLevel}vh`,
-    '--stratosphere-size': `${6428 * zoomLevel}vh`,
-    '--mesophere-size': `${6458 * zoomLevel}vh`,
-    '--thermosphere-size': `${7078 * zoomLevel}vh`,
-    '--exosphere-size': `${16378 * zoomLevel}vh`,
+    '--earth-size': `${earhOrbit * zoomLevel}vh`,
+    '--troposphere-size': `${(earhOrbit+18) * zoomLevel}vh`,
+    '--stratosphere-size': `${(earhOrbit+50) * zoomLevel}vh`,
+    '--mesophere-size': `${(earhOrbit+80) * zoomLevel}vh`,
+    '--thermosphere-size': `${(earhOrbit+700) * zoomLevel}vh`,
+    '--exosphere-size': `${(earhOrbit+10000) * zoomLevel}vh`,
     '--moon-distance': `${300000 * zoomLevel}vh`,
+    '--iss-distance':`${(earhOrbit+400) * zoomLevel}vh`,
     '--moon-size': `${1737 * zoomLevel}vh`,
+    '--iss-size': `${30 * zoomLevel}vh`,
     '--mesosphere-left-shift': `${getLayerLeftShift(6378 * zoomLevel, 6458 * zoomLevel)}vh`,
     '--troposphere-left-shift': `${getLayerLeftShift(6378 * zoomLevel, 6390 * zoomLevel)}vh`,
     '--stratosphere-left-shift': `${getLayerLeftShift(6378 * zoomLevel, 6428)}vh`,
     '--earth-opacity': Math.min(1, Math.max(0.4, 1 - (Math.log(zoomLevel * Math.E) + 6) / 6)),
+    '--iss-orbit-opacity': Math.min(0.2, ((Math.log(zoomLevel * Math.E) + 6) / 6)-0.3),
   };
 
   return (
     <div className="atmosphere-container">
       <input
         type="range" // these ranges have shown to cover the most region when it comes to interesting zones and distances of orbit
-        min="-6"
+        min="-5.8"
         max="1.5"
         step="0.01"
         value={Math.log(zoomLevel * Math.E)} // Inverse of the logZoom calculation
@@ -70,11 +73,13 @@ const EarthAtmosphere = () => {
         <div className="scale-label">{getScaleLength()}</div>
       </div>
       <div className="atmosphere-layers" style={zoomStyle}>
+        <div className="layer moon"/>
+        <div className="layer moon-orbit" />
         <div className="layer layer-exosphere" />
         <div className="layer layer-thermosphere" />
+        <div className="layer iss" />
+        <div className="layer iss-orbit" />
         <div className="layer layer-mesosphere" />
-        <div className="layer moon" />
-        <div className="layer moon-orbit" />
         <div className="layer layer-stratosphere" />
         <div className="layer layer-troposphere" />
         <div className="earth" />
